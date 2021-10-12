0cd3493790ee0c05ce1586c17278923b29950b20
yiqing <yiqing.zhang@leyantech.com>
2021-10-12 15:39:39 +0800

    整合 SpringTask 实现定时任务

---
+++ src/main/java/zone/yiqing/mall/config/SpringTaskConfig.java
@@ -0,0 +1,15 @@
+package zone.yiqing.mall.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * 定时任务配置. 只需添加@EnableScheduling注解即可开启.
+ * @author yiqing.zhang, {@literal <yiqing.zhang@leyantech.com>}
+ * @date 2021-10-12.
+ */
+@Configuration
+@EnableScheduling
+public class SpringTaskConfig {
+
+}
